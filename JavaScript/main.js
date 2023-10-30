6def9c91705725ca5d2d00e6a43d4a28c537a27a
Mursaleen Nisar <146980038+SuhaibNisar19@users.noreply.github.com>
2023-10-30 20:30:59 +0530

    School Website

---
+++ JavaScript/main.js
@@ -0,0 +1,22 @@
+// Change NavBar style on scroll
+
+window.addEventListener("scroll", () => {
+    document.querySelector("nav").classList.toggle("window-scroll", window.scrollY > 0)
+})
+
+// Show/Hide FAQ Answer
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("open");
+
+        // Change icon
+        const icon = faq.querySelector(".faq_icon i")
+        if (icon.className === "uil uil-plus") {
+            icon.className = "uil uil-minus";
+        } else {
+            icon.className = "uil uil-plus";
+        }
+    })
+})
