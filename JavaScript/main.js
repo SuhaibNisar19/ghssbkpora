f590a79aac52a6412c3e895dcf8739ff74b1e142
Mursaleen Nisar <146980038+SuhaibNisar19@users.noreply.github.com>
2023-11-01 22:59:00 +0530

    Debugged some minor bugs, styled the blank pages and linked the blank buttons

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,59 +5,78 @@ var modal = document.getElementById("myModal");
 
 // Function to display the modal
 function openModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 // Function to close the modal
 function closeModal() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 // Display the modal when the page loads (you can adjust the timing)
-window.onload = function() {
-    openModal();
-}
-
+window.onload = function () {
+  openModal();
+};
 
 // Change NavBar style on scroll
 
 window.addEventListener("scroll", () => {
-    document.querySelector("nav").classList.toggle("window-scroll", window.scrollY > 0)
-})
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
 
 // Show/Hide FAQ Answer
 
 const faqs = document.querySelectorAll(".faq");
-faqs.forEach(faq => {
-    faq.addEventListener("click", () => {
-        faq.classList.toggle("open");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
 
-        // Change icon
-        const icon = faq.querySelector(".faq_icon i")
-        if (icon.className === "uil uil-plus") {
-            icon.className = "uil uil-minus";
-        } else {
-            icon.className = "uil uil-plus";
-        }
-    })
-})
+    // Change icon
+    const icon = faq.querySelector(".faq_icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
 
 // Show/Hide NavBar Menu
-const menu = document.querySelector(".nav_menu")
-const menuBtn = document.querySelector("#open_menu_btn")
-const closeBtn = document.querySelector("#close_menu_btn")
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open_menu_btn");
+const closeBtn = document.querySelector("#close_menu_btn");
 
 menuBtn.addEventListener("click", () => {
-    menu.style.display = "flex";
-    closeBtn.style.display = "inline-block";
-    menuBtn.style.display = "none";
-})
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
 
 // Close Nav Menu
 const closeNav = () => {
-    menu.style.display = "none";
-    closeBtn.style.display = "none";
-    menuBtn.style.display = "inline-block";
-}
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
 
-closeBtn.addEventListener("click", closeNav)
+closeBtn.addEventListener("click", closeNav);
+
+
+// Smooth scroll effect from button click to a section
+document.querySelectorAll(".smooth_scroll").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById("curriculum");
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  });
+});
